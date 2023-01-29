70fbdd776fb530c0a57bdfb11dc542064f2e54bd
linshizhi <linshizhi@laihua.com>
2023-01-29 18:03:56 +0800

    Stage.

--- src/Master/machine.h
+++ src/Master/machine.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <memory>
 #include "message.h"
+#include <sys/socket.h>
 
 #ifndef MACHINE_H
 #define MACHINE_H
@@ -9,10 +10,31 @@
 
 class Machine {
 public:
+    virtual ~Machine() {}
+    virtual bool connect() = 0;
 
 private:
-    void sendMsg(std::shared_ptr<Message> msg);
+    virtual void sendMsg(std::shared_ptr<Message> msg) = 0;
+    std::string ident;
 };
 
+
+class GenericMachine: public Machine {
+public:
+    GenericMachine(std::string addr, int port) {
+
+    }
+
+    bool connect() final {
+
+    }
+
+private:
+    void sendMsg(std::shared_ptr<Message> msg) final {
+
+    }
+
+    int socket;
+};
 
 #endif /* MACHINE_H */
--- src/message.h
+++ src/message.h
@@ -11,14 +11,13 @@
 class Message {
 public:
     virtual ~Message() {};
-    virtual int toByte(std::shared_ptr<uint8_t> buffer) = 0;
+    virtual int toByte(std::shared_ptr<uint8_t> &buffer) = 0;
 };
-
-class JSONMessage: public Message {};
 
 class BinaryMessage: public Message {
 public:
-    BinaryMessage(std::shared_ptr<uint8_t> msg_, size_t len): msg(nullptr), length(0)  {
+    BinaryMessage(std::shared_ptr<uint8_t> msg_, size_t len):
+        Message(), msg(nullptr), length(0)  {
 
         if (msg_ == nullptr) {
             throw std::invalid_argument("Message pointer is null");
@@ -27,10 +26,10 @@ public:
         msg = std::make_shared<uint8_t>(len);
         std::memcpy(msg.get(), msg_.get(), len);
     }
-    ~BinaryMessage() {}
 
-    int toByte(std::shared_ptr<uint8_t> buffer) final {
-        return 0;
+    int toByte(std::shared_ptr<uint8_t> &buffer) final {
+        buffer = msg;
+        return length;
     }
 private:
     std::shared_ptr<uint8_t> msg;
